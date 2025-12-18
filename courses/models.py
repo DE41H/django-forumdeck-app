43a20d3ce3b7ad9985f68f0bc141b20fa1532dc0
sreyash <sreyash.s@outlook.com>
2025-12-18 21:17:11 +0530

    made the ORM models more robust

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 
@@ -8,7 +9,10 @@ class Department(models.Model):
         verbose_name = 'Department'
         verbose_name_plural = 'Departments'
 
-    name = models.CharField(verbose_name='name')
+    name = models.CharField(verbose_name='name', max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return f'Department Name: {self.name}'
 
 
 class Course(models.Model):
@@ -16,10 +20,19 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'Course'
         verbose_name_plural = 'Courses'
+        ordering = ['code']
 
-    code = models.CharField(verbose_name='code')
-    title = models.CharField(verbose_name='title')
+    code_validator = validators.RegexValidator(
+        regex=r'^[A-Z]{2,4}\s[A-Z]{1}\d{3}$',
+        message='Course code must in the format "CS F111" or "MATH F101"'
+    )
+
+    code = models.CharField(verbose_name='code', max_length=25, unique=True, validators=[code_validator])
+    title = models.CharField(verbose_name='title', max_length=255)
     department = models.ForeignKey(to='courses.Department', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'Course Code: {self.code}'
 
 
 class Resource(models.Model):
@@ -27,12 +40,17 @@ class Resource(models.Model):
     class Meta:
         verbose_name = 'Resource'
         verbose_name_plural = 'Resources'
+        unique_together = ['course', 'title']
 
-    class types(models.TextChoices):
+    class TypeChoices(models.TextChoices):
         PDF = 'PDF', 'PDF'
         VIDEO = 'VIDEO', 'Video'
         LINK = 'LINK', 'Link'
+    
+    course = models.ForeignKey(verbose_name='course', to='courses.Course', on_delete=models.CASCADE, related_name='resources')
+    title = models.CharField(verbose_name='title', max_length=255)
+    type = models.CharField(verbose_name='type', choices=TypeChoices.choices, max_length=5)
+    link = models.URLField(verbose_name='link', max_length=200, unique=True)
 
-    title = models.CharField(verbose_name='title')
-    type = models.CharField(verbose_name='type', choices=types.choices)
-    link = models.URLField(verbose_name='link')
+    def __str__(self) -> str:
+        return f'Resource Title: {self.title}\nType: {self.type}\nLink: {self.link}'
--- threads/models.py
+++ threads/models.py
@@ -1,22 +1,48 @@
 from django.db import models
 from django.conf import settings
-from django.utils import timezone
+from django.core import exceptions, validators
+from django.utils import timezone, text
 
 # Create your models here.
+
+class ActiveManager(models.Manager):
+
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(is_deleted=False)
+
 
 class Thread(models.Model):
 
     class Meta:
         verbose_name = 'Thread'
         verbose_name_plural = 'Threads'
-
-    category = models.ForeignKey(verbose_name='category', to='threads.Category', on_delete=models.CASCADE)
-    title = models.CharField(verbose_name='title')
-    content = models.CharField(verbose_name='content')
+        ordering = ['-created_at']
+    
+    objects = models.Manager()
+    active = ActiveManager()
+    
+    upvotes = models.ManyToManyField(verbose_name='upvotes', to=settings.AUTH_USER_MODEL, blank=True, related_name='upvoted_threads')
+    upvote_count = models.PositiveIntegerField(verbose_name='upvote count', default=0)
+    category = models.ForeignKey(verbose_name='category', to='threads.Category', on_delete=models.CASCADE, related_name='threads')
+    title = models.CharField(verbose_name='title', max_length=255, db_index=True)
+    raw_content = models.TextField(verbose_name='raw content')
     author = models.ForeignKey(verbose_name='author', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='threads')
-    created_at = models.DateTimeField(verbose_name='created at', default=timezone.now)
-    tagged_courses = models.ManyToManyField(verbose_name='tagged courses', to='courses.Course')
-    tagged_documents = models.ManyToManyField(verbose_name='tagged documents', to='courses.Resource')
+    created_at = models.DateTimeField(verbose_name='created at', default=timezone.now, db_index=True)
+    tagged_courses = models.ManyToManyField(verbose_name='tagged courses', to='courses.Course', blank=True, related_name='tagged')
+    tagged_documents = models.ManyToManyField(verbose_name='tagged documents', to='courses.Resource', blank=True, related_name='tagged')
+    tags = models.ManyToManyField(verbose_name='tags', to='threads.Tag', blank=True, related_name='tagged')
+    is_deleted = models.BooleanField(verbose_name='is deleted', default=False)
+    is_locked = models.BooleanField(verbose_name='is locked', default=False)
+
+    @property
+    def content(self):
+        if self.is_deleted:
+            return '[This content has been removed by a moderator]'
+        else:
+            return str(self.raw_content)
+
+    def __str__(self) -> str:
+        return f'Thread Title: {self.title}\nAuthor: {self.author}\nContent: {self.content}'
 
 
 class Category(models.Model):
@@ -25,7 +51,38 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(verbose_name='name')
+    name = models.CharField(verbose_name='name', max_length=255, unique=True)
+    slug = models.SlugField(unique=True, blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = text.slugify(self.name)
+            count = 1
+            while Category.objects.filter(slug=f'slug-{count}').exists():
+                count += 1
+            self.slug = f'{slug}-{count}'
+        super().save(*args, **kwargs)
+        
+    def __str__(self) -> str:
+        return f'Category Name: {self.name}'
+
+
+class Tag(models.Model):
+    
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    hex_validator = validators.RegexValidator(
+        regex=r'^#[A-Za-z0-9]{6}$',
+        message='Enter a valid hex color code'
+    )
+
+    name = models.CharField(verbose_name='name', max_length=255, unique=True)
+    color = models.CharField(verbose_name='color', max_length=7, validators=[hex_validator])
+
+    def __str__(self) -> str:
+        return f'Tag Name: {self.name}\nTag Color: {self.color}'
 
 
 class Reply(models.Model):
@@ -33,7 +90,51 @@ class Reply(models.Model):
     class Meta:
         verbose_name = 'Reply'
         verbose_name_plural = 'Replies'
+        ordering = ['thread', '-created_at']
 
+    objects = models.Manager()
+    active = ActiveManager()
+
+    thread = models.ForeignKey(verbose_name='thread', to='threads.Thread', on_delete=models.CASCADE, related_name='replies')
+    upvotes = models.ManyToManyField(verbose_name='upvotes', to=settings.AUTH_USER_MODEL, blank=True, related_name='upvoted_replies')
+    upvote_count = models.PositiveIntegerField(verbose_name='upvote count', default=0)
     author = models.ForeignKey(verbose_name='author', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='replies')
-    content = models.CharField(verbose_name='content')
-    created_at = models.DateTimeField(verbose_name='created at', default=timezone.now)
+    raw_content = models.TextField(verbose_name='raw content')
+    created_at = models.DateTimeField(verbose_name='created at', default=timezone.now, db_index=True)
+    is_deleted = models.BooleanField(verbose_name='is deleted', default=False)
+
+    @property
+    def content(self):
+        if self.is_deleted:
+            return '[This content has been removed by a moderator]'
+        else:
+            return str(self.raw_content)
+
+    def __str__(self) -> str:
+        return f'Reply to: {self.thread}\nAuthor: {self.author}\nContent: {self.content}'
+
+
+class Report(models.Model):
+
+    class Meta:
+        verbose_name = 'Report'
+        verbose_name_plural = 'Reports'
+
+    class StatusChoices(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        RESOLVED = 'RESOLVED', 'Resolved'
+
+    reporter = models.ForeignKey(verbose_name='reporter', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reports')
+    thread = models.ForeignKey(verbose_name='thread', to='threads.Thread', on_delete=models.CASCADE, related_name='reports', blank=True, null=True)
+    reply = models.ForeignKey(verbose_name='reply', to='threads.Reply', on_delete=models.CASCADE, related_name='reports', blank=True, null=True)
+    reason = models.TextField(verbose_name='reason')
+    status = models.CharField(verbose_name='status', choices=StatusChoices.choices, max_length=8, default=StatusChoices.PENDING)
+    
+    def clean(self) -> None:
+        if not self.thread and not self.reply:
+            raise exceptions.ValidationError('A report must be linked to either a Reply or Thread')
+        elif self.thread and self.reply:
+            raise exceptions.ValidationError('A report cannot be linked to both a Reply and a Thread at the same time')
+
+    def __str__(self) -> str:
+        return f'Report By: {self.reporter}\nReport On: {self.thread if self.thread else self.reply}\nReason: {self.reason}\nStatus: {self.status}'
